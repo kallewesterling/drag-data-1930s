1da3add5819976b74009b6b95609a7d84078e60c
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2020-11-17 09:52:13 -0500

    Adding a lot of data cleanup functions

--- process.py
+++ process.py
@@ -1,3 +1,5 @@
+from difflib import SequenceMatcher
+import re
 import networkx as nx
 import pandas as pd
 import json
@@ -5,14 +7,16 @@ from datetime import datetime
 from drag.settings import SPREADSHEET, DB_SPREADSHEET, START_YEAR, END_YEAR, CLEANING, CACHE
 from pathlib import Path
 
+
 class Place():
 
-    if not CACHE.exists(): CACHE.mkdir(parents=True)
+    if not CACHE.exists():
+        CACHE.mkdir(parents=True)
 
     from geopy.geocoders import Nominatim
     geolocator = Nominatim(user_agent="place-app")
 
-    def __init__(self, name:str):
+    def __init__(self, name: str):
         self.name = name
         self.cache = CACHE / f'{self.name}.json'
 
@@ -102,16 +106,23 @@ for row in df.fillna('').itertuples():
             continue
 
     # strip off any trailing or leading spaces
-    if club_display:  club_display = club_display.strip()
-    if club_id:       club_id = club_id.strip()
-    if city:          city = city.strip()
-    if performer:     performer = performer.strip()
-    if revue_name:    revue_name = revue_name.strip()
+    if club_display:
+        club_display = club_display.strip()
+    if club_id:
+        club_id = club_id.strip()
+    if city:
+        city = city.strip()
+    if performer:
+        performer = performer.strip()
+    if revue_name:
+        revue_name = revue_name.strip()
 
     add = list()
     if club_id and city:
-        current_weight = graph.get_edge_data(club_id, city, default={}).get('weight')
-        current_found = graph.get_edge_data(club_id, city, default={}).get('found', [])
+        current_weight = graph.get_edge_data(
+            club_id, city, default={}).get('weight')
+        current_found = graph.get_edge_data(
+            club_id, city, default={}).get('found', [])
         if current_weight == None:
             add.append((club_id, city, 1))
             found = [source]
@@ -121,8 +132,10 @@ for row in df.fillna('').itertuples():
             found.append(source)
     else:
         if performer and city:
-            current_weight = graph.get_edge_data(performer, city, default={}).get('weight')
-            current_found = graph.get_edge_data(performer, city, default={}).get('found', [])
+            current_weight = graph.get_edge_data(
+                performer, city, default={}).get('weight')
+            current_found = graph.get_edge_data(
+                performer, city, default={}).get('found', [])
 
             if current_weight == None:
                 add.append((performer, city, 1))
@@ -133,8 +146,10 @@ for row in df.fillna('').itertuples():
                 found.append(source)
 
     if club_id and performer:
-        current_weight = graph.get_edge_data(performer, club_id, default={}).get('weight')
-        current_found = graph.get_edge_data(performer, club_id, default={}).get('found', [])
+        current_weight = graph.get_edge_data(
+            performer, club_id, default={}).get('weight')
+        current_found = graph.get_edge_data(
+            performer, club_id, default={}).get('found', [])
 
         if current_weight == None:
             add.append((performer, club_id, 1))
@@ -149,14 +164,12 @@ for row in df.fillna('').itertuples():
     except NameError:
         found = []
 
-
     graph.add_weighted_edges_from(
         add,
         found=list(set(found)),
         revue_name=revue_name,
         date=date.strftime("%Y-%m-%d")
     )
-
 
     attrs = {
         city: {
@@ -213,14 +226,12 @@ def set_centralities(graph):
         }
         nx.set_node_attributes(graph, attrs)
 
-
     # 1000x degree centrality measure for each node
     for data in [{node: degree_central*1000} for node, degree_central in nx.degree_centrality(graph).items()]:
         attrs = {
             list(data.keys())[0]: {'1000x-degree-centrality': list(data.values())[0]},
         }
         nx.set_node_attributes(graph, attrs)
-
 
     # 1000x closeness centrality measure for each node
     for data in [{node: close_central*1000} for node, close_central in nx.closeness_centrality(graph).items()]:
@@ -229,7 +240,6 @@ def set_centralities(graph):
         }
         nx.set_node_attributes(graph, attrs)
 
-
     # 1000x betweenness centrality measure for each node
     for data in [{node: between_central*1000} for node, between_central in nx.betweenness_centrality(graph).items()]:
         attrs = {
@@ -237,12 +247,149 @@ def set_centralities(graph):
         }
         nx.set_node_attributes(graph, attrs)
 
-
     return(graph)
+
 
 graph = set_degrees(graph)
 graph = set_centralities(graph)
 
 
+json_data = nx.node_link_data(graph)
+
+
+################################################
+## Testing for similarities in names          ##
+################################################
+
+
+def clean_name(name):
+    name = name.strip()
+
+    if re.search(r'\s+(\(.*\))', name):
+        name = re.sub(r'\s+(\(.*\))', '', name)
+
+    if name.endswith('?'):
+        name = name[:-1]
+
+    if name.startswith('['):
+        name = name[1:]
+    if name.endswith(']'):
+        name = name[:-1]
+    if name.startswith('*'):
+        name = name[1:]
+    if name.endswith('*'):
+        name = name[:-1]
+
+    if name.endswith('?'):
+        name = name[:-1]
+
+    return name
+
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+
+def find_similar(name, list_of_names, threshold=0.75):
+    _ = list()
+
+    lower_list_of_names = [x.lower() for x in list_of_names]
+
+    original_name = name.lower()
+    # we only want to keep names that are longer than 2 characters
+    if len(original_name) < 4:
+        names = [x.lower() for x in name.split(' ') if len(x) > 2]
+        full_name = ' '.join(names)
+    else:
+        names = [x.lower() for x in name.split(' ')]
+        full_name = original_name
+    for name in names:
+        for i, test_name in enumerate(lower_list_of_names):
+            if name in test_name:
+                if test_name == full_name:
+                    continue
+                sim = similar(full_name, test_name)
+                if sim > threshold:
+                    # print(test_name, f'({full_name}) =', )
+                    _.append(list_of_names[i])
+    return(_)
+
+
+def load_G():
+    with open('/Users/kallewesterling/Google Drive/Dropbox (snapshot)/dev/dev-dissertation/Drag data for 1930s/docs/drag-data-for-1930s.json', 'r') as f:
+        return nx.json_graph.node_link_graph(json.load(f))
+
+
+def edges_connected_to(edge='Jean Malin', G=None):
+    if not G:
+        G = load_G()
+    connected_to = list()
+    for node in [x[1] for x in G.out_edges(edge)]:
+        connected_to.extend([x[0] for x in G.in_edges(node) if x[0] != edge])
+        connected_to.extend([x[0] for x in G.out_edges(node) if x[0] != edge])
+    for node in [x[1] for x in G.in_edges(edge)]:
+        connected_to.extend([x[0] for x in G.in_edges(node) if x[0] != edge])
+        connected_to.extend([x[0] for x in G.out_edges(node) if x[0] != edge])
+    _ = sorted(list(set(connected_to)))
+    return _
+
+
+def neighboring_nodes(edge1, edge2, G=None, sort=True):
+    set1 = set(edges_connected_to(edge1, G))
+    set2 = set(edges_connected_to(edge2, G))
+    if not sort:
+        return list(set1.intersection(set2))
+    return sorted(list(set1.intersection(set2)))
+
+
+all_names = list()
+
+for node in json_data.get('nodes'):
+    if node.get('category') == 'performer':
+        all_names.append(node.get('id'))
+
+print('---------------------')
+
+for name in all_names:
+    test_list = all_names[:]
+    test_list.remove(name)
+
+    name = clean_name(name)
+    similar_names = find_similar(name, test_list)
+    if similar_names:
+        print(
+            f"Warning: {name} seems to have similar names:\n         {', '.join(similar_names)}")
+
+print('---------------------')
+
+for name in all_names:
+    partial, full = list(), list()
+    name_split = name.split(' ')
+    neighbors = edges_connected_to(name, G=graph)
+    for node in neighbors:
+        for node_test in name_split:
+            if node_test.lower().strip() in [x.lower().strip() for x in node.split(' ')]:
+                if node.lower().strip() == node_test.lower().strip():
+                    # full similar node found in neighborhood
+                    full.append(node)
+                else:
+                    # partial similar node found in neighborhood
+                    partial.append(node)
+
+    if partial:
+        print(
+            f'Warning: Partial similar nodes to {name} found in its neighborhood:\n         {", ".join(partial)}')
+
+    if full:
+        print(
+            f'Warning: FULL similar nodes to {name} found:\n         {", ".join(full)}\n')
+
+    if full or partial:
+        neighbors = "\n         - ".join(neighbors)
+        print(f'-------> Neighbors:\n         - {neighbors}')
+        print('\n')
+
+print('---------------------')
+
 # write file
-Path('./docs/drag-data-for-1930s.json').write_text(json.dumps(nx.node_link_data(graph)))
+Path('./docs/drag-data-for-1930s.json').write_text(json.dumps(json_data))
