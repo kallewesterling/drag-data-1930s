0d9033980065fbd738775ce5eedfa6785df33878
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2021-06-10 18:31:21 -0300

    Changing to `V1` of dataset

--- generate-cooccurrence-data.py
+++ generate-cooccurrence-data.py
@@ -56,7 +56,7 @@ def slugify(value, allow_unicode=False, verbose=False):
 
 def get_raw_data(verbose=True):
     df = pd.read_csv(
-        "https://docs.google.com/spreadsheets/d/e/2PACX-1vT0E0Y7txIa2pfBuusA1cd8X5OVhQ_D0qZC8D40KhTU3xB7McsPR2kuB7GH6ncmNT3nfjEYGbscOPp0/pub?gid=0&single=true&output=csv"
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vT0E0Y7txIa2pfBuusA1cd8X5OVhQ_D0qZC8D40KhTU3xB7McsPR2kuB7GH6ncmNT3nfjEYGbscOPp0/pub?gid=95950987&single=true&output=csv"
     )
 
     df.replace("—", "", inplace=True)
