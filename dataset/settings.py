8667a056e983b067ac749a64fbbcf1dbb515ac85
Kalle Westerling <7298727+kallewesterling@users.noreply.github.com>
2021-08-31 17:08:05 -0300

    Switching `v1` url to better dataset

--- dataset/settings.py
+++ dataset/settings.py
@@ -1,4 +1,4 @@
 class Settings:
     CACHE_FILE_DIR = ".cache/"  # Must end with /
     CACHE_FILE = ".dataset"
-    URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT0E0Y7txIa2pfBuusA1cd8X5OVhQ_D0qZC8D40KhTU3xB7McsPR2kuB7GH6ncmNT3nfjEYGbscOPp0/pub?gid=95950987&single=true&output=csv"
+    URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT0E0Y7txIa2pfBuusA1cd8X5OVhQ_D0qZC8D40KhTU3xB7McsPR2kuB7GH6ncmNT3nfjEYGbscOPp0/pub?gid=254069133&single=true&output=csv"
--- generate-cooccurrence-data.py
+++ generate-cooccurrence-data.py
@@ -24,7 +24,7 @@ settings = {"DAYSPANS": [3, 14, 31, 93, 186, 365]}
 urls = [
     {
         "prefix": "v1",
-        "url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vT0E0Y7txIa2pfBuusA1cd8X5OVhQ_D0qZC8D40KhTU3xB7McsPR2kuB7GH6ncmNT3nfjEYGbscOPp0/pub?gid=95950987&single=true&output=csv",
+        "url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vT0E0Y7txIa2pfBuusA1cd8X5OVhQ_D0qZC8D40KhTU3xB7McsPR2kuB7GH6ncmNT3nfjEYGbscOPp0/pub?gid=254069133&single=true&output=csv",
     },
     {
         "prefix": "live",
